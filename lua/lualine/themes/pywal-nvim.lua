4744987b87e97fc96810c69d7d73812e2e3a4a79
Manuel Brea <m.brea.carreras@gmail.com>
2022-03-04 17:12:07 +0000

    Fix shading logic

--- lua/lualine/themes/pywal-nvim.lua
+++ lua/lualine/themes/pywal-nvim.lua
@@ -1,38 +1,38 @@
-local core = require('pywal.core')
+local core = require("pywal.core")
 local colors = core.get_colors()
 
 local pywal = {}
 
 pywal.normal = {
   a = { bg = colors.color4, fg = colors.background },
-  b = { bg = colors.background, fg = colors.color7 },
-  c = { bg = colors.background, fg = colors.foreground },
+  b = { bg = colors.shades.background.dark, fg = colors.color7 },
+  c = { bg = colors.shades.background.dark, fg = colors.foreground },
 }
 
 pywal.insert = {
   a = { bg = colors.color2, fg = colors.background },
-  b = { bg = colors.background, fg = colors.color4 },
+  b = { bg = colors.shades.background.dark, fg = colors.color4 },
 }
 
 pywal.command = {
   a = { bg = colors.color5, fg = colors.background },
-  b = { bg = colors.background, fg = colors.color5 },
+  b = { bg = colors.shades.background.dark, fg = colors.color5 },
 }
 
 pywal.visual = {
   a = { bg = colors.color6, fg = colors.background },
-  b = { bg = colors.background, fg = colors.color6 },
+  b = { bg = colors.shades.background.dark, fg = colors.color6 },
 }
 
 pywal.replace = {
   a = { bg = colors.color11, fg = colors.background },
-  b = { bg = colors.background, fg = colors.color11 },
+  b = { bg = colors.shades.background.dark, fg = colors.color11 },
 }
 
 pywal.inactive = {
   a = { bg = colors.background, fg = colors.color7 },
-  b = { bg = colors.background, fg = colors.foreground, gui = "bold" },
-  c = { bg = colors.background, fg = colors.foreground },
+  b = { bg = colors.shades.background.dark, fg = colors.foreground, gui = "bold" },
+  c = { bg = colors.shades.background.dark, fg = colors.foreground },
 }
 
 return pywal
