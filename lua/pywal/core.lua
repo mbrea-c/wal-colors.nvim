64f7c0551fc03eb239ae8c18066f48fd88a8b3f9
Manuel Brea <m.brea.carreras@gmail.com>
2022-03-04 16:42:26 +0000

    Added shading logic

--- lua/pywal/core.lua
+++ lua/pywal/core.lua
@@ -1,9 +1,75 @@
 local M = {}
 
-function M.get_colors()
-  vim.cmd [[ source $HOME/.cache/wal/colors-wal.vim ]]
+local function parse_rgb(s)
+  assert(s[1] == "#")
+  local r = tonumber(s:sub(2, 3), 16)
+  local g = tonumber(s:sub(4, 5), 16)
+  local b = tonumber(s:sub(6, 7), 16)
+  print(r, g, b)
+  return r, g, b
+end
 
-  return {
+local function rgb_to_hsv(r, g, b)
+  print(r, g, b)
+  local K = 0
+  if g < b then
+    g, b = b, g
+    K = -1
+  end
+  if r < g then
+    r, g = g, r
+    K = -2 / 6 - K
+  end
+  local chroma = r - math.min(g, b)
+  local h = math.abs(K + (g - b) / (6 * chroma + 1e-20))
+  local s = chroma / (r + 1e-20)
+  local v = r
+  return h * 360, s, v
+end
+
+local function hsv_to_rgb(h, s, v)
+  if s == 0 then --gray
+    return v, v, v
+  end
+  local H = h / 60
+  local i = math.floor(H) --which 1/6 part of hue circle
+  local f = H - i
+  local p = v * (1 - s)
+  local q = v * (1 - s * f)
+  local t = v * (1 - s * (1 - f))
+  if i == 0 then
+    return v, t, p
+  elseif i == 1 then
+    return q, v, p
+  elseif i == 2 then
+    return p, v, t
+  elseif i == 3 then
+    return p, q, v
+  elseif i == 4 then
+    return t, p, v
+  else
+    return v, p, q
+  end
+end
+
+local function darken(v, ratio)
+  local h, s, v = rgb_to_hsv(parse_rgb(v))
+  print(h, s, v)
+  local r, g, b = hsv_to_rgb(h, s, ratio * v)
+  local light = string.format("#%2hhX%2hhX%2hhX", r, g, b)
+end
+
+local function lighten(v, ratio)
+  local h, s, v = rgb_to_hsv(parse_rgb(v))
+  print(h, s, v)
+  local r, g, b = hsv_to_rgb(h, s, 100 - ratio * (100 - v))
+  local light = string.format("#%2hhX%2hhX%2hhX", r, g, b)
+end
+
+function M.get_colors()
+  vim.cmd([[ source $HOME/.cache/wal/colors-wal.vim ]])
+
+  local colors = {
     background = vim.g.background,
     foreground = vim.g.foreground,
     cursor = vim.g.cursor,
@@ -24,6 +90,15 @@ function M.get_colors()
     color14 = vim.g.color14,
     color15 = vim.g.color15,
   }
+
+  colors.shades = {}
+
+  for k, v in pairs(colors) do
+    colors.shades[k] = { light = lighten(v, 0.8), normal = v, dark = darken(v, 0.8) }
+    print(colors.shades[k])
+  end
+
+  return colors
 end
 
 return M
