e04f05c542b10c16afc27760ac766840772f0169
Manuel Brea <m.brea.carreras@gmail.com>
2022-10-06 13:57:51 +0100

    fix

--- lua/pywal/config.lua
+++ lua/pywal/config.lua
@@ -232,12 +232,12 @@ M.highlights_base = function(colors)
   set_hl("TelescopeSelection", { fg = colors.background, bg = colors.color1 })
 
   -- Indent Blank Line
-  set_hl("IndentBlanklineChar", { fg = colors.color1, bg = colors.background, nocombine = true })
-  set_hl("IndentBlanklineIndent1", { bg = colors.shades.background.dark, nocombine = true })
-  set_hl("IndentBlanklineIndent2", { bg = colors.background, nocombine = true })
-  local context_line_hl = colors.shades.background.lighter
-  set_hl("IndentBlanklineContextSpaceChar", { fg = context_line_hl, bg = context_line_hl, nocombine = true })
-  set_hl("IndentBlanklineContextChar", { fg = context_line_hl, bg = context_line_hl, nocombine = true })
+  set_hl("IndentBlanklineChar", { fg = colors.color1, bg = colors.background })
+  set_hl("IndentBlanklineIndent1", { bg = colors.shades.background.dark })
+  set_hl("IndentBlanklineIndent2", { bg = colors.background })
+  local context_line_hl = colors.shades.background.light
+  set_hl("IndentBlanklineContextSpaceChar", { fg = context_line_hl, bg = context_line_hl })
+  set_hl("IndentBlanklineContextChar", { fg = colors.shades.color4.darker, bg = context_line_hl })
 
   -- NvimTree
   set_hl("NvimTreeNormal", { fg = colors.foreground, bg = colors.background })
