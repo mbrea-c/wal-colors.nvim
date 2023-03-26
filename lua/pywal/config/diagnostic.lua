49c917119d7191f8ab98c3d73145e3a0d581cd1e
Manuel Brea <m.brea.carreras@gmail.com>
2023-03-26 15:57:06 +0100

    Fix diagnostic highlights

--- lua/pywal/config/diagnostic.lua
+++ lua/pywal/config/diagnostic.lua
@@ -1,14 +1,19 @@
 local M = {}
 
 M.setup = function(set_hl, get_hl, colors)
-  set_hl("DiagnosticError", { bg = colors.shades.background.dark, fg = colors.shades.color1.lightest })
-  set_hl("DiagnosticWarn", { bg = colors.shades.background.dark, fg = colors.shades.color3.lightest })
-  set_hl("DiagnosticInfo", { bg = colors.shades.background.dark, fg = colors.shades.foreground.lightest })
-  set_hl("DiagnosticHint", { bg = colors.shades.background.dark, fg = colors.shades.color4.lightest })
-  set_hl("DiagnosticUnderlineError", { link = "DiagnosticError" })
-  set_hl("DiagnosticUnderlineWarn", { link = "DiagnosticWarn" })
-  set_hl("DiagnosticUnderlineInfo", { link = "DiagnosticInfo" })
-  set_hl("DiagnosticUnderlineHint", { link = "DiagnosticHint" })
+  local error = colors.shades.color1.lightest
+  local warning = colors.shades.color3.lightest
+  local info = colors.shades.foreground.lightest
+  local hint = colors.shades.color4.lightest
+
+  set_hl("DiagnosticError", { underdotted = true, bg = colors.shades.background.dark, fg = error })
+  set_hl("DiagnosticWarn", { underdotted = true, bg = colors.shades.background.dark, fg = warning })
+  set_hl("DiagnosticInfo", { underdotted = true, bg = colors.shades.background.dark, fg = info })
+  set_hl("DiagnosticHint", { underdotted = true, bg = colors.shades.background.dark, fg = hint })
+  set_hl("DiagnosticUnderlineError", { italic = true, underdotted = true, sp = error })
+  set_hl("DiagnosticUnderlineWarn", { italic = true, underdotted = true, sp = warning })
+  set_hl("DiagnosticUnderlineInfo", { italic = true, underdotted = true, sp = info })
+  set_hl("DiagnosticUnderlineHint", { italic = true, underdotted = true, sp = hint })
   --DiagnosticVirtualTextError
   --DiagnosticVirtualTextWarn
   --DiagnosticVirtualTextInfo
