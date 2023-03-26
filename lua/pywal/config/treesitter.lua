4d6c9d3a02a9df20eb893bd2ec2069654e9dc8ad
Manuel Brea <m.brea.carreras@gmail.com>
2023-03-26 23:09:17 +0100

    Added highlight for symbol

--- lua/pywal/config/treesitter.lua
+++ lua/pywal/config/treesitter.lua
@@ -28,32 +28,32 @@ M.setup = function(set_hl, get_hl, colors)
   -- @constMacro        = { };    -- For constants that are defined by macros: `NULL` in C.
   -- @error             = { };    -- For syntax/parser errors.
   -- @exception         = { };    -- For exception related keywords.
-  set_hl("@field", { fg = colors.color12 }) -- For fields
+  set_hl("@field", { fg = colors.color12 })                -- For fields
   -- @float             = { };    -- For floats.
   set_hl("@function", { fg = colors.color4, bold = true }) -- For function (calls and definitions))
   -- @funcBuiltin       = { };    -- For builtin functions: `table.insert` in Lua.
   -- @funcMacro         = { };    -- For macro defined fuctions (calls and definitions): each `macro_rules` in Rust.
   -- @include           = { };    -- For includes: `#include` in C, `use` or `extern crate` in Rust, or `require` in Lua.
-  set_hl("@keyword", { fg = colors.color2, bold = true }) -- For keywords that don't fall in previous categories.
+  set_hl("@keyword", { fg = colors.color2, bold = true })                                       -- For keywords that don't fall in previous categories.
   set_hl("@keyword.function", { fg = colors.shades.color2.darker, bold = true, italic = true }) -- For keywords used to define a fuction.
-  set_hl("@label", { fg = colors.color7 }) -- For labels: `label:` in C and `:label:` in Lua.
+  set_hl("@label", { fg = colors.color7 })                                                      -- For labels: `label:` in C and `:label:` in Lua.
   -- @method            = { };    -- For method calls and definitions.
-  set_hl("@namespace", { fg = colors.color6, bold = true }) -- For identifiers referring to modules and namespaces.
+  set_hl("@namespace", { fg = colors.color6, bold = true })                                     -- For identifiers referring to modules and namespaces.
   -- @none              = { };    -- TODO: docs
-  set_hl("@number", { fg = colors.shades.color1.light }) -- For all numbers
-  set_hl("@operator", { fg = colors.color3 }) -- For any operator: `+`, but also `->` and `*` in C.
-  set_hl("@operator", { fg = colors.color3, bold = true }) -- For keyword operators: `not`, but also `in` and `is` in Python.
-  set_hl("@parameter", { fg = colors.color4 }) -- For parameters of a function.
+  set_hl("@number", { fg = colors.shades.color1.light })                                        -- For all numbers
+  set_hl("@operator", { fg = colors.color3 })                                                   -- For any operator: `+`, but also `->` and `*` in C.
+  set_hl("@operator", { fg = colors.color3, bold = true })                                      -- For keyword operators: `not`, but also `in` and `is` in Python.
+  set_hl("@parameter", { fg = colors.color4 })                                                  -- For parameters of a function.
   -- @parameterReference= { };    -- For references to parameters of a function.
-  set_hl("@property", { fg = colors.color4 }) -- Same as `@field`.
-  set_hl("@punctuation.delimiter", { fg = colors.shades.color3.darker }) -- For delimiters ie: `.`
-  set_hl("@punctuation.bracket", { fg = colors.foreground }) -- For brackets and parens.
-  set_hl("@punctuation.special", { fg = colors.color3 }) -- For special punctutation that does not fall in the catagories before.
-  set_hl("@repeat", { fg = colors.color2 }) -- For keywords related to loops.
-  set_hl("@string", { fg = colors.color1 }) -- For strings.
-  set_hl("@string.regex", { fg = colors.color7 }) -- For regexes.
-  set_hl("@string.escape", { fg = colors.color6 }) -- For escape characters within a string.
-  -- @symbol            = { };    -- For identifiers referring to symbols or atoms.
+  set_hl("@property", { fg = colors.color4 })                                                   -- Same as `@field`.
+  set_hl("@punctuation.delimiter", { fg = colors.shades.color3.darker })                        -- For delimiters ie: `.`
+  set_hl("@punctuation.bracket", { fg = colors.foreground })                                    -- For brackets and parens.
+  set_hl("@punctuation.special", { fg = colors.color3 })                                        -- For special punctutation that does not fall in the catagories before.
+  set_hl("@repeat", { fg = colors.color2 })                                                     -- For keywords related to loops.
+  set_hl("@string", { fg = colors.color1 })                                                     -- For strings.
+  set_hl("@string.regex", { fg = colors.color7 })                                               -- For regexes.
+  set_hl("@string.escape", { fg = colors.color6 })                                              -- For escape characters within a string.
+  set_hl("@symbol", { fg = colors.shades.foreground.darker })                                   -- For escape characters within a string.
   set_hl("@type", { fg = colors.color2 })
   set_hl("@type.qualifier", { fg = colors.shades.color2.light, italic = true })
   set_hl("@type.builtin", { fg = colors.shades.color2.dark })
@@ -65,15 +65,15 @@ M.setup = function(set_hl, get_hl, colors)
   -- Text
   -- [x] Complete
   -- [ ] Assigned
-  set_hl("@text", { fg = colors.shades.foreground.dark }) -- For strings considered text in a markup language.
-  set_hl("@text.emphasis", { italic = true }) -- For text to be represented strong.
-  set_hl("@text.strong", { bold = true }) -- For text to be represented with emphasis.
-  set_hl("@text.underline", { underline = true }) -- For text to be represented with an underline.
-  set_hl("@text.strike", { strikethrough = true }) -- For strikethrough text.
-  set_hl("@text.title", { bold = true }) -- Text that is part of a title.
-  set_hl("@text.literal", nil) -- Literal text.
+  set_hl("@text", { fg = colors.shades.foreground.dark })                      -- For strings considered text in a markup language.
+  set_hl("@text.emphasis", { italic = true })                                  -- For text to be represented strong.
+  set_hl("@text.strong", { bold = true })                                      -- For text to be represented with emphasis.
+  set_hl("@text.underline", { underline = true })                              -- For text to be represented with an underline.
+  set_hl("@text.strike", { strikethrough = true })                             -- For strikethrough text.
+  set_hl("@text.title", { bold = true })                                       -- Text that is part of a title.
+  set_hl("@text.literal", nil)                                                 -- Literal text.
   set_hl("@text.uri", { fg = colors.shades.color4.darkest, underline = true }) -- Any URI like a link or email.
-  set_hl("@text.math", { fg = colors.color1 }) -- For latex math
+  set_hl("@text.math", { fg = colors.color1 })                                 -- For latex math
   set_hl("@text.reference", { fg = colors.color8 })
   set_hl("@text.environment", { fg = colors.color6 })
   set_hl("@text.environment.name", { fg = colors.color6 })
