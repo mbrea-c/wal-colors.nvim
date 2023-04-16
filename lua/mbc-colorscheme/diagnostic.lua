0ba4922ff6ba929aa6c7696b7168db746d4be1ba
Manuel Brea <m.brea.carreras@gmail.com>
2023-04-16 20:26:48 +0100

    Added more screenshots, saturated highlights for diagnostics to make them stand out

--- lua/mbc-colorscheme/diagnostic.lua
+++ lua/mbc-colorscheme/diagnostic.lua
@@ -1,10 +1,10 @@
 local M = {}
 
 M.setup = function(set_hl, get_hl, colors)
-  local error = colors.red:lightened(0.3)
-  local warning = colors.yellow:lightened(0.3)
-  local info = colors.foreground:lightened(0.3)
-  local hint = colors.blue:lightened(0.3)
+  local error = colors.red:lightened(0.3):saturated(0.3)
+  local warning = colors.yellow:lightened(0.3):saturated(0.3)
+  local info = colors.foreground:lightened(0.5)
+  local hint = colors.blue:lightened(0.3):saturated(0.3)
 
   set_hl("DiagnosticError", { underdotted = true, bg = colors.background:darkened(0.1), fg = error })
   set_hl("DiagnosticWarn", { underdotted = true, bg = colors.background:darkened(0.1), fg = warning })
